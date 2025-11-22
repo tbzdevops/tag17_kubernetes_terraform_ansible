4f8390c712b333f1c514d6b42655d32a2eff91dd
Armin Doerzbach <armin@doerzbach.com>
2025-11-22 15:15:26 +0100

    backend.conf instead of hardcoding

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "ado8-terraform-state-bucket-1"
     key            = "kubernetes/state.tfstate"
-    region         = "us-east-1"
-#    dynamodb_table = "terraform-lock-table"  # optional für Locking
     encrypt        = true
   }
 }
